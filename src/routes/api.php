13b6338110cecc17174ae67dcc518aad9f6366b9
hayrican <hayricanbarcin@gmail.com>
2019-10-30 16:37:32 +0300

    getCredit request method changed

--- src/routes/api.php
+++ src/routes/api.php
@@ -3,5 +3,5 @@
 
 Route::group(['prefix'=>config('codecfastsms.route_prefix'),'namespace' => 'HayriCan\CodecFastSms\Controllers','middleware' => config('codecfastsms.middleware')], function () {
     Route::post('codec-fastsms', 'CodecFastSmsController@postSmsVariables');
-    Route::post('codec-credit', 'CodecFastSmsController@getCredit');
+    Route::get('codec-credit', 'CodecFastSmsController@getCredit');
 });
